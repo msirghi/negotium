b5ae610498a4b5ead6041e33083a72043c2f84e9
sirghim <mihail.sirghi@reedexpo.co.uk>
2022-01-02 14:04:26 +0000

    fix: minor refactor

--- pages/home/projects/[id].tsx
+++ pages/home/projects/[id].tsx
@@ -1,36 +1,19 @@
 import { ProjectContainer } from '../../../src/components/project';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { StaticProps } from '../../../src/common/constants/types';
 import {GetServerSidePropsContext, GetStaticPaths} from 'next';
-import i18config from '../../../next-i18next.config';
+import { withTranslation } from 'next-i18next';
 
 const Project = () => {
   return <ProjectContainer />;
 };
 
-export const getServerSideProps = async ({ locale }: GetServerSidePropsContext) => {
+export async function getServerSideProps({
+  locale,
+}: GetServerSidePropsContext) {
   return {
     props: {
-      ...(await serverSideTranslations(locale || 'en', ['common', 'settings'], i18config)),
+      ...(await serverSideTranslations(locale || 'en', ['common', 'settings'])),
     },
   };
-};
-
-// export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
-//   return {
-//     paths: [], //indicates that no page needs be created at build time
-//     fallback: 'blocking', //indicates the type of fallback
-//   };
-// };
-
-// export const getStaticProps: GetStaticPaths<{ slug: string }> = async () => {
-//   return {
-//     paths: [],
-//     fallback: 'blocking',
-//     props: {
-//       ...(await serverSideTranslations(locale, ['common', 'settings'])),
-//     },
-//   };
-// };
-
+}
 export default Project;
