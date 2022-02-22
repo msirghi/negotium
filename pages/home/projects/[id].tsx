eb895699b05360cf7d9fc60a3b8b80e09d82ace3
sirghim <mihail.sirghi@reedexpo.co.uk>
2022-02-22 20:04:11 +0000

    chore: migration from next-18next to next-translate

--- pages/home/projects/[id].tsx
+++ pages/home/projects/[id].tsx
@@ -1,9 +1,13 @@
 import { ProjectContainer } from '../../../src/components/project';
+import { GetStaticPaths } from 'next';
 
 export default function Project() {
   return <ProjectContainer />;
 }
 
-export async function getStaticProps() {
-  return { props: {} };
-}
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: 'blocking', //indicates the type of fallback
+  };
+};
