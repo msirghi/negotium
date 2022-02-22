896aa384b2365a4899b8132140339455a276d599
sirghim <mihail.sirghi@reedexpo.co.uk>
2022-02-22 17:28:43 +0000

    chore: changed next config to fallback to path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { fs: false, path: false };
     return config;
   },
 };
