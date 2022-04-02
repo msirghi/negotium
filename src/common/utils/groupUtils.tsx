457f6f0d8f76663af50d306ea753a3f07dc92a10
sirghim <mihail.sirghi@reedexpo.co.uk>
2022-04-02 15:49:44 +0100

    fix: lodash.groupBy import

--- src/common/utils/groupUtils.tsx
+++ src/common/utils/groupUtils.tsx
@@ -1,11 +1,11 @@
 import { Task, TimelineGroup } from '../types/tasks.types';
-// @ts-ignore
-import groupBy from 'lodash.groupBy';
 import dayjs from 'dayjs';
 import SortUtils from './sortUtils';
 
+const groupBy = require('lodash.groupby');
+
 const groupTasksByDay = (tasks: Task[]): TimelineGroup[] => {
-  const groupedTasks = groupBy(tasks, (task) => dayjs(task.dueDate).startOf('day').format());
+  const groupedTasks = groupBy(tasks, (task: Task) => dayjs(task.dueDate).startOf('day').format());
   const modifiedTasks = Object.keys(groupedTasks).map((key) => ({ date: key, tasks: groupedTasks[key] }));
   return modifiedTasks.sort((i, y) => SortUtils.sortByDate(i.date, y.date));
 };
